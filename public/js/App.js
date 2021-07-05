ebcb91b34511c01ebe10ee830518f7712f7ddd46
Jim Sharples <jamie.sharples@gmail.com>
2021-07-05 21:51:22 +1000

    Added local storage util

--- public/js/App.js
+++ public/js/App.js
@@ -1,38 +1,56 @@
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-import logger from './util/SimpleDebug.js';
-import Controller from "./Controller.js";
-
-var App = /*#__PURE__*/function (_React$Component) {
-  _inheritsLoose(App, _React$Component);
-
-  function App() {
-    var _this;
-
-    _this = _React$Component.call(this) || this;
-    logger.setOn();
-    logger.setLevel(100);
-    _this.controller = new Controller(_assertThisInitialized(_this), window.localStorage);
-    return _this;
-  }
-
-  var _proto = App.prototype;
-
-  _proto.render = function render() {
-    return /*#__PURE__*/React.createElement("div", {
-      id: "App",
-      className: "App"
-    }, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
-  };
-
-  return App;
-}(React.Component);
-
-var element = /*#__PURE__*/React.createElement(App, {
-  className: "container-fluid"
+System.register(["./util/SimpleDebug.js", "./Controller.js"], function (exports_1, context_1) {
+    "use strict";
+    var __extends = (this && this.__extends) || (function () {
+        var extendStatics = function (d, b) {
+            extendStatics = Object.setPrototypeOf ||
+                ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+                function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+            return extendStatics(d, b);
+        };
+        return function (d, b) {
+            if (typeof b !== "function" && b !== null)
+                throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+            extendStatics(d, b);
+            function __() { this.constructor = d; }
+            d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+        };
+    })();
+    var SimpleDebug_js_1, Controller_js_1, App, element;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (SimpleDebug_js_1_1) {
+                SimpleDebug_js_1 = SimpleDebug_js_1_1;
+            },
+            function (Controller_js_1_1) {
+                Controller_js_1 = Controller_js_1_1;
+            }
+        ],
+        execute: function () {
+            //  import * as React from "react";
+            //  import * as ReactDOM from "react-dom";
+            // @ts-ignore
+            App = /** @class */ (function (_super) {
+                __extends(App, _super);
+                function App() {
+                    var _this = 
+                    // @ts-ignore
+                    _super.call(this) || this;
+                    SimpleDebug_js_1.default.setOn();
+                    SimpleDebug_js_1.default.setLevel(100);
+                    _this.controller = new Controller_js_1.default(_this, window.localStorage);
+                    return _this;
+                }
+                App.prototype.render = function () {
+                    return (React.createElement("div", { id: "App", className: "App" },
+                        React.createElement("h1", null, "Hello World!")));
+                };
+                return App;
+            }(React.Component));
+            // @ts-ignore
+            element = React.createElement(App, { className: "container-fluid" });
+            // @ts-ignore
+            ReactDOM.render(element, document.getElementById("root"));
+        }
+    };
 });
-ReactDOM.render(element, document.getElementById("root"));
--- public/js/util/FetchUtil.js
+++ public/js/util/FetchUtil.js
@@ -1,60 +1,65 @@
-import logger from './SimpleDebug.js';
-
-var FetchUtil = /*#__PURE__*/function () {
-  function FetchUtil() {}
-
-  var _proto = FetchUtil.prototype;
-
-  /*
-    Utility function for calling JSON POST requests
-    Parameters:
-    1.  URL to send the POST request too;
-    2.  parameters object whose attribute (name/values) are the request parameters; and
-    3.  A function to receive the results when the fetch has completed
-        The callback function should have the following form
-        callback (jsonDataReturned, httpStatusCode)
-        a)  A successful fetch will return the JSON data in the first parameter and a status code of the server
-        b)  Parameters that cannot be converted to JSON format will give a null data and code 404
-        c)  A server error will give that code and no data
-  */
-  _proto.fetchQLJSON = function fetchQLJSON(url, parameters, callback) {
-    logger.log("Executing fetch with URL " + url + " with body " + parameters, 100);
-
-    try {
-      JSON.stringify({
-        parameters: parameters
-      });
-    } catch (error) {
-      logger.log("Unable to convert parameters to JSON", 100);
-      logger.log(parameters, 100);
-      callback(null, 404);
-    }
-
-    var postParameters = {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        parameters: parameters
-      })
+System.register(["./SimpleDebug.js"], function (exports_1, context_1) {
+    "use strict";
+    var SimpleDebug_js_1, FetchUtil;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (SimpleDebug_js_1_1) {
+                SimpleDebug_js_1 = SimpleDebug_js_1_1;
+            }
+        ],
+        execute: function () {
+            FetchUtil = /** @class */ (function () {
+                function FetchUtil() {
+                }
+                /*
+                  Utility function for calling JSON POST requests
+                  Parameters:
+                  1.  URL to send the POST request too;
+                  2.  parameters object whose attribute (name/values) are the request parameters; and
+                  3.  A function to receive the results when the fetch has completed
+                      The callback function should have the following form
+                      callback (jsonDataReturned, httpStatusCode)
+                      a)  A successful fetch will return the JSON data in the first parameter and a status code of the server
+                      b)  Parameters that cannot be converted to JSON format will give a null data and code 404
+                      c)  A server error will give that code and no data
+                */
+                FetchUtil.fetchQLJSON = function (url, parameters, callback) {
+                    SimpleDebug_js_1.default.log("Executing fetch with URL " + url + " with body " + parameters, 100);
+                    try {
+                        JSON.stringify({ parameters: parameters });
+                    }
+                    catch (error) {
+                        SimpleDebug_js_1.default.log("Unable to convert parameters to JSON", 100);
+                        SimpleDebug_js_1.default.log(parameters, 100);
+                        callback(null, 404);
+                    }
+                    var postParameters = {
+                        method: "POST",
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ parameters: parameters })
+                    };
+                    fetch(url, postParameters)
+                        .then(function (response) {
+                        SimpleDebug_js_1.default.log("Response code was " + response.status);
+                        if (response.status >= 200 && response.status <= 299) {
+                            return response.json();
+                        }
+                        else {
+                            callback(null, response.status);
+                            throw new Error("no results");
+                        }
+                    })
+                        .then(function (data) {
+                        callback(JSON.parse(data), 200);
+                    })
+                        .catch(function (error) {
+                        callback(null, 404);
+                    });
+                };
+                return FetchUtil;
+            }());
+            exports_1("default", FetchUtil);
+        }
     };
-    fetch(url, postParameters).then(function (response) {
-      logger.log("Response code was " + response.status);
-
-      if (response.status >= 200 && response.status <= 299) {
-        return response.json();
-      } else {
-        callback(null, response.status);
-        throw new Error("no results");
-      }
-    }).then(function (data) {
-      callback(JSON.parse(data));
-    }).catch(function (error) {});
-  };
-
-  return FetchUtil;
-}();
-
-var fetchUtil = new FetchUtil();
-export default fetchUtil;
+});
