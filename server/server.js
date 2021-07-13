f2b35a6dbe0202ea0ec845f7a0152f10ef7b1ffd
jsharples777 <jamie.sharples@gmail.com>
2021-07-13 13:29:30 +1000

    Added simple socket chat

--- server/server.js
+++ server/server.js
@@ -2,27 +2,32 @@
 const express = require('express');
 const request = require('request');
 const morgan = require('morgan');
-const bodyparser = require('body-parser');
-const {Server} = require("socket.io");
-const http = require('http');
-const dotenv = require('dotenv')
+const bodyParser = require('body-parser');
 
-const app = express();
-const httpServer = http.createServer(app);
-const io = new Server(httpServer,{});
+var dotenv = require('dotenv')
+
+var app = express();
+var httpServer = require('http').Server(app);
+
 dotenv.config();
 
-const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
+var wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
 
-io.use(wrap(bodyparser.json())); /* handle JSON POST */
-io.use(wrap(morgan("dev"))); /* log server calls with performance timinig */
+// parse application/x-www-form-urlencoded
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var io = require("socket.io")(httpServer);
+
+//io.use(wrap(morgan("dev"))); /* log server calls with performance timinig */
 
 /* log call requests with body */
-io.use(wrap((request, response, next) => {
-    console.log(`Received request for ${request.url} with/without body`);
-    console.log(request.body);
-    next();
-}));
+// io.use(wrap((request, response, next) => {
+//     console.log(`Received request for ${request.url} with/without body`);
+//     console.log(request.body);
+//     next();
+// }));
 
 /* setup the public files to be available (e.g. content, css, client side js files) */
 app.use('/',express.static("public"));
@@ -32,6 +37,22 @@ app.use('/dist', express.static("dist"));
 app.get("/test", (req, res) => {
     console.log("url: " + req.url);
     res.send("Hello World");
+});
+
+/* handle request for current weather from Open Weather API */
+app.post("/current", (req, res) => {
+    console.log("url: " + req.url);
+    console.log("body: " + req.body);
+    console.log("body: " + req.body.parameters);
+    let newURL = process.env.CURRENT_WEATHER_URL + "?q=" + req.body.parameters.q + "&appid=" + process.env.API_KEY + "&units=metric";
+    console.log("new URL is: " + newURL);
+    request(newURL, function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+        res.status(response.statusCode);
+        res.json(body);
+    });
 });
 
 /* handle forecast retrieval from Open Weather API */
